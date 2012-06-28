7345a476800d19069891fb63e7bff779032d805c
Eliot <saltycrane@gmail.com>
2012-06-28 14:54:27 -0700

    Use Emacs 24.1 and Pymacs 0.25
     - Use emacs 24.1
     - Install pymacs 0.25
     - Don't track xhtml-loader.rnc
     - Change font size for emacs 24.1

--- create-virtualenv.sh
+++ create-virtualenv.sh
@@ -1,7 +1,41 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Create a virtualenv named "venv" for use with Emacs.
+#
+# Usage:
+#    ./create-virtualenv <path to python executable>
+#
+# Example:
+#    ./create-virtualenv $HOME/lib/python-2.7.2/bin/python
 
-virtualenv venv
-venv/bin/pip install https://github.com/pinard/Pymacs/tarball/v0.23
+set -x
+set -e
+
+if [ "$#" -ne 1 ]
+then
+    echo "Error: script takes 1 argument"
+    exit 1
+fi
+
+# Create virtualenv
+virtualenv --python=$1 venv
+
+# Install Pymacs 0.25
+source venv/bin/activate
+tempdir=$(mktemp -d)
+pushd $tempdir
+wget -O tmp.tar.gz https://github.com/pinard/Pymacs/tarball/v0.25
+tar zxvf tmp.tar.gz
+pushd pinard-Pymacs-5989046
+make install
+popd
+popd
+rm -rf $tempdir
+deactivate
+
+# Install other Python packages
 venv/bin/pip install http://bitbucket.org/agr/ropemacs/get/tip.tar.gz
-venv/bin/pip install pyflakes
-venv/bin/pip install pep8
+venv/bin/pip install pyflakes==0.5.0
+venv/bin/pip install pep8==1.0.1
+
+set +x
