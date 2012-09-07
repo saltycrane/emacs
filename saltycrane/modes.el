930e71d2514681d80729af840994cf998a635f12
Eliot <saltycrane@gmail.com>
2012-09-07 10:47:47 -0700

    Dont automatically compile scss files

--- saltycrane/modes.el
+++ saltycrane/modes.el
@@ -290,3 +290,4 @@
 ;;================================================
 (autoload 'scss-mode "scss-mode-2d25dec870")
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(setq scss-compile-at-save nil)
