3ab74f56a25370cdeb7305b8fb8112047357b37a
Eliot <saltycrane@gmail.com>
2013-03-20 13:47:03 -0700

    Change modeline to mode-line for new emacs

--- saltycrane/appearance.el
+++ saltycrane/appearance.el
@@ -2,8 +2,8 @@
 ;; COLORS
 ;; see http://www.gnu.org/software/emacs/manual/html_node/emacs/Standard-Faces.html
 ;;================================================
-;;(set-face-background 'modeline "#4477aa")
-(set-face-background 'modeline-inactive "#99aaff")
+(set-face-background 'mode-line "#4477aa")
+;; (set-face-background 'mode-line-inactive "#99aaff")
 (set-face-background 'fringe "#809088")
 (set-face-background 'trailing-whitespace "#e0e0e0")
 (set-face-background 'scroll-bar "#4477aa") ;; this doesn't work
