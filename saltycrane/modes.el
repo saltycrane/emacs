3b1e966775d9150e9fdaa72402cb6528403b7847
Eliot <saltycrane@gmail.com>
2013-03-22 15:20:23 -0700

    Try autopair instead of electric-pair-mode

--- saltycrane/modes.el
+++ saltycrane/modes.el
@@ -343,7 +343,14 @@
  )
 (add-to-list 'auto-mode-alist '("COMMIT_EDITMSG$" . git-commit-mode))
 
+;; ;;================================================
+;; ;; ELECTRIC-PAIR-MODE
+;; ;;================================================
+;; (electric-pair-mode)
+
 ;;================================================
-;; ELECTRIC-PAIR-MODE
+;; AUTOPAIR-MODE
 ;;================================================
-(electric-pair-mode)
+(add-to-list 'load-path (concat sc-vendor-dir "/capitaomorte-autopair-d2346ce7f6"))
+(require 'autopair)
+(autopair-global-mode) ;; enable autopair in all buffers
