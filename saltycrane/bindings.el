e7d0b8826aa7d1df7553f8dc31542b1ec04eb347
Eliot <saltycrane@gmail.com>
2013-03-22 12:42:10 -0700

    bind RET to newline-and-indent (bad idea?)

--- saltycrane/bindings.el
+++ saltycrane/bindings.el
@@ -21,6 +21,7 @@
 (global-set-key "\C-p" 'kill-line)
 (global-set-key "\M-v" 'downcase-word)
 (global-set-key "\M-b" 'recenter)
+(global-set-key "\C-m" 'newline-and-indent)  ;; experimental
 
 ;; per Steve Yegge's suggestion
 (global-set-key "\C-x\C-n" 'execute-extended-command)
