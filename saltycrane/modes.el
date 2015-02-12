67479ccc786d8bed76442c257b8db5817dec9039
Eliot <saltycrane@gmail.com>
2015-02-12 11:30:11 -0800

    use Shift + Right Mouse click for fold/unfolding blocks in web-mode

--- saltycrane/modes.el
+++ saltycrane/modes.el
@@ -164,6 +164,7 @@
     ))
 (define-key web-mode-map (kbd "M-;") 'sc-jinja-comment-dwim)
 (define-key web-mode-map (kbd "C-c /") 'web-mode-element-close)
+(define-key web-mode-map [S-mouse-3] 'web-mode-fold-or-unfold)
 
 ;;================================================
 ;; COFFEE-MODE
