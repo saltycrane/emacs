ff335b71f9497e63d7daf06afa44470313203c7a
Eliot <saltycrane@gmail.com>
2015-03-27 22:26:39 -0700

    add settings for JSX

--- saltycrane/modes.el
+++ saltycrane/modes.el
@@ -184,6 +184,15 @@
 (define-key web-mode-map [S-mouse-3] 'web-mode-fold-or-unfold)
 (define-key web-mode-map (kbd "C-'") 'web-mode-mark-and-expand)
 
+;; JSX (ReactJS)
+;; http://truongtx.me/2014/03/10/emacs-setup-jsx-mode-and-jsx-syntax-checking/
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+
 ;;================================================
 ;; COFFEE-MODE
 ;;================================================
